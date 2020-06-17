df287f3785a0c1da5e293f6d05bbda80f7d590f9
Nieto Pacheco Raul Abraham <MB45296@cb.bbvabancomer.com.mx>
2020-06-17 16:11:47 -0500

    ss

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.layout = html.Div([
     
     html.Div([
     html.H1("Mapa"),
-    html.Iframe(id="map",srcDoc=open("covid_por_mpio.html",encoding="utf8").read(),width="100%",height="400")
+    html.Iframe(id="map",srcDoc=open("covid_por_mpio.html",encoding="utf8").read(),width="100%",height="600")
         
     ]),
         
